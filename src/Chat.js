65f0a378056d90dfc632c55e4cbb620f7911ecbc
paramanicksoumyadeep-hash <paramanicksoumyadeep@gmail.com>
2026-02-28 14:59:11 +0530

    Update Chat.js

--- src/Chat.js
+++ src/Chat.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import ScrollToBottom from "react-scroll-to-bottom";
-import "./Chat.css";
+import "./chat.css";
 
 function Chat({ socket, username, room }) {
   const [currentMessage, setCurrentMessage] = useState("");
